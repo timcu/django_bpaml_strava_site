a2977adea54a33eed6339dd240e1cb87474365bc
D Tim Cummings <tim@triptera.com.au>
2025-09-30 19:20:45 +1000

    Add functionality to "view actviities for an athlete"

--- django_bpaml_strava/urls.py
+++ django_bpaml_strava/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from django_bpaml_strava.views import index_page, athlete_page
-
+from django_bpaml_strava.views import index_page, athlete_page, view_activities
 urlpatterns = [
   path('', index_page, name='index'),
   path('athlete/<str:strava_id>/', athlete_page, name='athlete'),
+  path('view-activities/athlete/<int:strava_id>', view_activities, name='view-activities'),
 ]
--- django_bpaml_strava/views.py
+++ django_bpaml_strava/views.py
@@ -1,6 +1,9 @@
 from allauth.socialaccount.models import SocialAccount
 from django.shortcuts import render, get_object_or_404
-from django_bpaml_strava.models import User
+from django.db.models import Prefetch
+from django.contrib.auth.decorators import login_required
+from django_bpaml_strava.models import Activity, User
+
 
 def index_page(request):
     """Find all athletes """
@@ -8,8 +11,30 @@ def index_page(request):
     context = {'athletes': list_social_accounts}
     return render(request, 'django_bpaml_strava/athletes.html', context)
 
+
 def athlete_page(request, strava_id):
     """Find just the one athlete with the supplied strava id"""
     a = get_object_or_404(SocialAccount, provider='strava', uid=strava_id)
     context = {'athlete': a}
+    return render(request, 'django_bpaml_strava/athlete.html', context)
+
+
+def social_account_with_sorted_activities(strava_id):
+    social_account = (SocialAccount.objects.filter(uid=strava_id, provider='strava')
+                      .select_related("user")
+                      .prefetch_related(
+                          Prefetch(
+                            "user__activity_set",
+                            queryset=Activity.objects.order_by("start_time")
+                          )
+                        )
+                      .first())
+    return social_account
+
+
+@login_required
+def view_activities(request, strava_id):
+    # Get the stored, and possibly refreshed, access token for this athlete
+    social_account = social_account_with_sorted_activities(strava_id)
+    context = {'athlete': social_account}
     return render(request, 'django_bpaml_strava/athlete.html', context)
