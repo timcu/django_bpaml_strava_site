a2f20bb14a435089d96582382d5edae2d0482348
D Tim Cummings <tim@triptera.com.au>
2025-09-30 16:21:21 +1000

    Create a site index template with django admin styling

--- django_bpaml_strava_site/urls.py
+++ django_bpaml_strava_site/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bpaml-strava/', include('django_bpaml_strava.urls')),
+    path('', TemplateView.as_view(template_name="bpaml_home.html")),
 ]
